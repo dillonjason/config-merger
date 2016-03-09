b69323fe23e17b0551a32ad414015084f46b1e8d
David Moody <moody.david@gmail.com>
2016-03-09 08:33:57 -0500

    made some adjustments

--- index.js
+++ index.js
@@ -5,71 +5,11 @@ var fs = require('fs'),
     nconf = require('nconf'),
     readJson = require('read-package-json'),
     yargs = require('yargs').argv,
-    readR = require('./lib/read-recursive');
+    ConfigMerger = require('./lib/config-merger.js');
 
+var readRecursive = require('./lib/read-recursive');
 var NAME = 0;
 var ENV = 2;
-
-function noDotFiles(x) {
-    return x[0] !== '.' && x.split('.')[x.split('.').length - 1] === 'json';
-}
-
-function Configify(options) {
-    this.options = Object.assign({
-        environment: ''
-    }, options);
-    this.baseDir = path.join(process.cwd(), this.settings().srcFolder);
-    this.outputFile = this.settings().output;
-    this.provider = new nconf.Provider().use('file', {file: path.join(this.baseDir, this.outputFile)});
-    this.provider.load();
-
-}
-
-Configify.prototype.getProp = function (filename, type) {
-    var names = filename.split('.');
-    if (type === NAME) {
-        return names[NAME];
-    }
-    if (type === ENV && names.length > 3) {
-        return names[ENV]
-    } else {
-        return ''
-    }
-};
-
-Configify.prototype.getConfigs = function (files, environment) {
-    var self = this;
-    return files.filter(function (file) {
-        return ((file !== self.outputFile) && (self.getProp(file, ENV) === self.options.environment))
-    });
-};
-
-Configify.prototype.settings = function () {
-    return this.options.config[this.options.configName];
-
-};
-
-Configify.prototype.loadConfigs = function(files){
-    var self = this;
-    self.getConfigs(files, '').forEach(function (file) {
-        console.log(path.join(self.baseDir, file))
-        console.log(self.getProp(file, NAME))
-        self.provider.merge(self.getProp(file, NAME), JSON.parse(fs.readFileSync(path.join(self.baseDir, file), 'utf8')));
-    })
-
-    self.getConfigs(files, process.env.NODE_ENV).forEach(function (file) {
-        console.log(path.join(self.baseDir, file))
-        console.log(self.getProp(file, NAME))
-        self.provider.merge(self.getProp(file, NAME), JSON.parse(fs.readFileSync(path.join(self.baseDir, file), 'utf8')));
-    });
-}
-
-Configify.prototype.save = function () {
-    var self = this;
-    var files = readR(self.baseDir, noDotFiles);
-    self.loadConfigs(files);
-    self.provider.save();
-};
 
 
 function run(){
@@ -78,7 +18,7 @@ function run(){
             console.error("There was an error reading the file");
             return
         }
-        var config = new Configify({
+        var config = new ConfigMerger(fs, path, nconf, readRecursive, {
             environment: process.env.NODE_ENV,
             configName: yargs.config,
             config: data.configmerger.find(function (item) {
---
+++ tests/configmerger.spec.js
@@ -0,0 +1,61 @@
+var assert = require('assert');
+
+
+var ConfigMerger = require('../lib/config-merger.js');
+
+
+
+describe("configify object", function(){
+  beforeEach(function(){
+    var path = {
+      join: function(){ return "/Users/davidmoody/Projects"; }
+    }
+    var fs = {}
+    var nconf = {
+      Provider: function(){
+        var thing = {
+          use: function() {
+            var thang = {
+              load: function() {
+                return true;
+              }
+            }
+            return thang;
+          }
+        }
+        return thing;
+      }
+    }
+
+    var reader = function(){
+      return [];
+    }
+    this.configMerger = new ConfigMerger(path, nconf, reader, {
+      environment: 'production',
+      configName: 'company1',
+      config:{
+        company1: {
+          output: "app.config.json",
+          srcFolder: "company1_configs"
+        }
+      }
+    });
+
+  });
+
+  it("can be newed up and ready to go", function(){
+    assert(!!this.configMerger == true);
+  });
+
+  it("should set the output param", function() {
+    assert(!!this.configMerger.settings().output == true)
+
+  });
+
+
+  it("should set the srcFolder param", function() {
+    assert(!!this.configMerger.settings().srcFolder == true);
+  });
+
+
+})
