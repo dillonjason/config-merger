9dff622cf2ca3630f86f04b077a60761161bc68e
dillon <djasuar@gmail.com>
2016-12-20 09:54:17 -0500

    Moved parameter validation into config-merger class

--- index.js
+++ index.js
@@ -5,65 +5,34 @@ var readPackageJson = require('read-package-json');
 
 function run(){
     readPackageJson('package.json', console.error, false, function (error, data) {
-        var missingParameter = [];
-        if (!yargs.source)
-            missingParameter.push('source');
+        if (!error && yargs.configName && data.configMerger && data.configMerger[yargs.configName]) {
+            var config = data.configMerger[yargs.configName];
+            config.source = config.source.length > 0 ? config.source : yargs.source;
+            config.environment = config.environment.length > 0 ? config.environment : yargs.environment;
+            config.output = config.output.length > 0 ? config.output : yargs.output;
 
-        if (!yargs.environment)
-            missingParameter.push('environment');
+            var config = new ConfigMerger({
+                source: config.source,
+                environment: config.environment,
+                output: config.output
+            });
 
-        if (!yargs.output)
-            missingParameter.push('output');
-
-        if (error && missingParameter.length > 0) {
-            console.error('Unable to find package.json and missing parameter(s): ' + missingParameter.join(', '));
-            return;
+            if (config.hasError)
+                console.error(config.errorMessage);
+            else
+                config.save();
         }
         else {
-            if (yargs.configName && data.configMerger && data.configMerger[yargs.configName]) {
-                var config = data.configMerger[yargs.configName];
-                config.source = config.source.length > 0 ? config.source : yargs.source;
-                config.environment = config.environment.length > 0 ? config.environment : yargs.environment;
-                config.output = config.output.length > 0 ? config.output : yargs.output;
+            var config = new ConfigMerger({
+                source: yargs.source,
+                environment: yargs.environment,
+                output: yargs.output
+            });
 
-                missingParameter = [];
-
-                if (!yargs.source)
-                    missingParameter.push('source');
-
-                if (!yargs.environment)
-                    missingParameter.push('environment');
-
-                if (!yargs.output)
-                    missingParameter.push('output');
-
-                if (missingParameter.length > 0) {
-                    console.error('Missing parameter(s): ' + missingParameter.join(', '));
-                    return;
-                }
-                else {
-                    var config = new ConfigMerger({
-                        source: config.source,
-                        environment: config.environment,
-                        output: config.output
-                    });
-
-                    config.save();
-                }
-            }
-            else if (missingParameter.length > 0) {
-                console.error('Missing parameter(s): ' + missingParameter.join(', '));
-                return;
-            }
-            else {
-                var config = new ConfigMerger({
-                    source: yargs.source,
-                    environment: yargs.environment,
-                    output: yargs.output
-                });
-
+            if (config.hasError)
+                console.error(config.errorMessage);
+            else
                 config.save();
-            }
         }
     });
 }
