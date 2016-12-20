58b6a1c7e6f8894410177dbc094aad25b661423b
dillon <djasuar@gmail.com>
2016-12-20 14:43:23 -0500

    Wrote tests for new config-merger

--- tests/configmerger.spec.js
+++ tests/configmerger.spec.js
@@ -1,73 +1,114 @@
 var assert = require('assert');
 var ConfigMerger = require('../lib/config-merger.js');
 var sinon = require('sinon');
+var fs = require('fs');
 
+describe("ConfigMerger", function () {
+    describe("Init", function () {
+        it("Should construct with all values in options", function () {
+            assert.doesNotThrow(function () {
+                new ConfigMerger({
+                    source: './configs',
+                    output: './test/app.config.json',
+                    environment: 'test'
+                })
+            });
+        });
 
+        it("Should have an error with missing parameter", function () {
+            var configMerger = new ConfigMerger({
+                output: './test/app.config.json',
+                environment: 'test'
+            });
 
-describe("configify object", function(){
-  function myPathStub(){
-    return {
-      join: function(){ return "/Users/davidmoody/Projects"; }
-    }
-  }
-  function myFsStub() {
-    return {};
-  }
-
-  function mynconfStub() {
-    return {
-      Provider: function(){
-        var thing = {
-          use: function() {
-            var thang = {
-              load: function() {
-                return true;
-              }
-            }
-            return thang;
-          }
-        }
-        return thing;
-      }
-    }
-  }
-  function myReaderStub() {
-    var reader = function(){
-      return [];
-    }
-  }
-  beforeEach(function(){
-    var fs = myFsStub();
-    var path = myPathStub();
-    var nconf = mynconfStub();
-    var reader = myReaderStub();
-
-    this.configMerger = new ConfigMerger(fs, path, nconf, reader, {
-      environment: 'production',
-      configName: 'company1',
-      config:{
-        company1: {
-          output: "app.config.json",
-          srcFolder: "company1_configs"
-        }
-      }
+            assert.equal(configMerger.hasError, true);
+        });
     });
 
-  });
+    describe("getMerged", function () {
+        var configMerger = new ConfigMerger({
+            source: './configs',
+            output: './test/app.config.json',
+            environment: 'test'
+        });
 
-  it("can be newed up and ready to go", function(){
-    assert(!!this.configMerger == true);
-  });
+        it("Should merge multiple base config without error", function () {
+            assert.doesNotThrow(function () {
+                configMerger.getMerged('/*.config.json');
+            });
+        });
 
-  it("should set the output param", function() {
-    assert(!!this.configMerger.settings().output == true)
+        it("Should merge single base config without error", function () {
+            assert.doesNotThrow(function () {
+                configMerger.getMerged('/config.json');
+            });
+        });
 
-  });
+        it("Should throw error when file not found", function () {
+            var configMerger = new ConfigMerger({
+                source: './configs',
+                output: './test/app.config.json',
+                environment: 'test'
+            });
 
+            configMerger.getMerged('not-a-file-name', function () {
+                assert.equal(configMerger.hasError, true);
+            });
+        });
+    });
 
-  it("should set the srcFolder param", function() {
-    assert(!!this.configMerger.settings().srcFolder == true);
-  });
+    describe('Save', function() {
+        var configMerger = new ConfigMerger({
+            source: './configs',
+            output: './test/app.config.json',
+            environment: 'test'
+        });
 
+        it("Should create and read without error", function () {
+            configMerger.save(function () {
+                fs.readFile('./test/app.config.json', function (error, context) {
+                    assert.equal(error, false);
+                })
+            });
+        });
 
-})
+        it("Should create config using just base", function () {
+            configMerger.save(function () {
+                fs.readFile('./test/app.config.json', function (error, context) {
+                    var obj = JSON.parse(context);
+                    assert.equal(obj.defaultError, "An unknown error has occurred");
+                })
+            });
+        });
+
+        it("Should create config that is not the base", function () {
+            configMerger.environment = "dev";
+            configMerger.save(function () {
+                fs.readFile('./test/app.config.json', function (error, context) {
+                    var obj = JSON.parse(context);
+                    assert.notEqual(obj.defaultError, "An unknown error has occurred");
+                })
+            });
+        });
+
+        it("Should create config using dev base", function () {
+            configMerger.environment = "dev";
+            configMerger.save(function () {
+                fs.readFile('./test/app.config.json', function (error, context) {
+                    var obj = JSON.parse(context);
+                    assert.equal(obj.defaultError, "An unknown error has a Development Issue");
+                })
+            });
+        });
+
+        it("Should create config using production base", function () {
+            configMerger.environment = "production";
+            configMerger.save(function () {
+                fs.readFile('./test/app.config.json', function (error, context) {
+                    var obj = JSON.parse(context);
+                    assert.equal(obj.defaultError, "An unknown error has a Production Issue");
+                })
+            });
+        });
+    })
+});
